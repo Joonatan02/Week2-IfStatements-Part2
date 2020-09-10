c97cbc70a3fb1cdb799b6d2e83399ed80403f87a
Opilane TTHK <opilane@tthk.ee>
2020-09-10 12:09:36 +0300

    Create Web Page

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string UserInput;
+
+            Console.WriteLine("What is your favourite animal?");
+            UserInput = Console.ReadLine();
+            
+            if(UserInput == "cat")
+            {
+                Console.WriteLine("You are a homebird");
+            }else if(UserInput == "dog")
+            {
+                Console.WriteLine("You are a partygoer");
+
+            }
+            else
+            {
+                Console.WriteLine($"You are a {UserInput} friend.");
+            }
+
+           
+
+            
+
+            
+
+
+        }
+
+
+    }
+}
